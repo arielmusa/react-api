0a6dc2b95c50de8c4e489cc8d2ef1dee5ca00651
arielmusa <arielmusainfo@gmail.com>
2025-05-30 15:32:52 +0200

    feat: get list of actors and actress and print in console/list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./assets/css/App.css";
 import Main from "./components/Main";
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,45 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const apiUrlActresses = "https://lanciweb.github.io/demo/api/actresses/";
+const apiUrlActors = "https://lanciweb.github.io/demo/api/actors/";
+
 export default function Main() {
-  return <h1>React API</h1>;
+  useEffect(() => {
+    axios
+      .get(apiUrlActors)
+      .then((res) => {
+        setActorsList(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  useEffect(() => {
+    axios
+      .get(apiUrlActresses)
+      .then((res) => {
+        setActressesList(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const [actorsList, setActorsList] = useState([]);
+  const [ActressesList, setActressesList] = useState([]);
+  console.log(actorsList);
+  console.log(ActressesList);
+  return (
+    <div className="container">
+      <div className="row">
+        <ul>
+          {actorsList.map((actor) => (
+            <li key={actor.id}>{actor.name}</li>
+          ))}
+        </ul>
+        <ul>
+          {ActressesList.map((actor) => (
+            <li key={actor.id}>{actor.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
